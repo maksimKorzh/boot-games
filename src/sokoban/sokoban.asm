934285ddc0e4011214cb839a541ecb19bf278646
Maksim Korzh <freesoft.for.people@gmail.com>
2021-09-20 08:38:41 +0300

    added collision detection in sokoban

--- src/sokoban/sokoban.asm
+++ src/sokoban/sokoban.asm
@@ -8,7 +8,7 @@
 [org 0x7c00]
 
               ; init video mode
-start:        mov ah, 0x00
+start         mov ah, 0x00
               mov al, 0x03
               int 0x10
 
@@ -21,45 +21,108 @@ start:        mov ah, 0x00
               mov bx, 0xb800
               mov es, bx
 
-              ; print map
-game_loop:    mov bh, 0       ; map flag
+              
+game_loop:    mov di,0x0b8
+              call clear_screen
+
+              mov bh, 0       ; map flag
               mov di, 0x4b8   ; print at center 548
               mov si, map
               call print_board
-and word [box + 4], 0x00ff
+
               ; print boxes
               mov bh, 1       ; boxes flag
               mov di, 0x4b8   ; print at center 548
               mov si, box
               call print_board
               
-              
-              
               mov ah, 0x00
               int 0x16
-              
-              
               
               cmp ah, 0x48
               je move_up
               
-              ;cmp ah, 0x01              
-              ;je quit
+              cmp ah, 0x50
+              je move_down
+              
+              cmp ah, 0x4b
+              je move_left
+              
+              cmp ah, 0x4d
+              je move_right
+              
                  
               jmp game_loop
 
-move_up:      
-              mov word [box + 4], 0x20ff
+move_up:      call clear_player
+              sub byte [player_row], 0x02
+              call set_player
+              call hit_wall
+              cmp cl, 1
+              je move_down
               jmp game_loop
+
+move_down:    call clear_player
+              add byte [player_row], 0x02
+              call set_player
+              call hit_wall
+              cmp cl, 1
+              je move_up
+              jmp game_loop
+
+move_left:    call clear_player
+              shl word [player_col], 1
+              call set_player
+              call hit_wall
+              cmp cl, 1
+              je move_right
+              jmp game_loop
+
+move_right:   call clear_player
+              shr word [player_col], 1
+              call set_player
+              call hit_wall
+              cmp cl, 1
+              je move_left
+              jmp game_loop
+
+hit_wall:     xor bx, bx
+              mov bl, byte [player_row]
+              mov cx, word [player_col]
+              and ch, byte [map + bx]
+              cmp ch, 0
+              jne true
+              mov cl, 0
+              ret
+
+true:         mov cl, 1
+              ret
 
 quit:         mov ah, 0x00
               mov al, 0x03
               int 0x10
               jmp 0x800:0x0000
-              
 
-print_board:  cld
+clear_player: xor bx, bx
+              mov bl, byte [player_row]
+              and word [box + bx], 0x00ff
+              ret
 
+set_player:   xor bx, bx
+              mov bl, byte [player_row]
+              mov cx, word [player_col]
+              or word [box + bx], cx
+              ret
+
+clear_screen: mov ax, 0x0000
+              stosw
+              cmp di, 0xb48
+              je done_clear
+              jmp clear_screen
+
+done_clear:   ret
+
+print_board:
 write_row:    add di, 0x90
               lodsw
               cmp al, 0xee
@@ -117,11 +180,14 @@ end_write:    ret
 
 
 player_row db 0x04
-player_col db 0x20
+player_col dw 0x2000
 map dw 0x003e, 0x00e2, 0x4082, 0x04e2, 0x40b2, 0x08a3, 0x1281, 0x0881, 0x00ff, 0x00ee
 box dw 0x0000, 0x0000, 0x2010, 0x0008, 0x0008, 0x0000, 0x005c, 0x0000, 0x0000, 0x00ee,
 
-times 512 - ($ - $$) db 0           ; fill trailing zeros to get exactly 512 bytes long binary file
+times 510 - ($ - $$) db 0           ; fill trailing zeros to get exactly 512 bytes long binary file
+dw 0xaa55
+
+times (2880 * 512) - ($-$$) db 0
 
 
 
