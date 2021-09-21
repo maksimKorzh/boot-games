906ae50b37cbf89247b8dcf0f0b9415e49315363
Maksim Korzh <freesoft.for.people@gmail.com>
2021-09-21 11:47:01 +0300

    rewriting sokoban from scratch, minimized print map routine

---
+++ src/sokoban/sokoban-old.asm
@@ -0,0 +1,266 @@
+;==========================
+;          PROG_1
+;==========================
+
+[bits 16]                           ; tell NASM to assemble 16-bit code
+[org 0x7c00]
+
+              ; video memory
+              mov bx, 0xb800
+              mov es, bx
+
+              
+game_loop:    mov di, 0x0000
+              call clear_screen
+
+              mov bh, 0       ; map flag
+              mov di, 0x4b8   ; print at center 548
+              mov si, map
+              call print_board
+
+              ; print boxes
+              mov bh, 1       ; boxes flag
+              mov di, 0x4b8   ; print at center 548
+              mov si, box
+              call print_board
+              
+              xor bx, bx
+              
+              mov ah, 0x00
+              int 0x16
+              
+              
+              
+              cmp ah, 0x48
+              je move_up
+              
+              cmp ah, 0x50
+              je move_down
+              
+              cmp ah, 0x4b
+              je move_left
+              
+              cmp ah, 0x4d
+              je move_right
+
+                 
+              jmp game_loop
+
+move_up:      call clear_player
+              sub byte [player_row], 0x02
+              call set_player
+              call collision
+              cmp cl, 1
+              je move_down
+              cmp dl, 1
+              je box_up
+              jmp game_loop
+
+move_down:    call clear_player
+              add byte [player_row], 0x02
+              call set_player
+              call collision
+              cmp cl, 1
+              je move_up
+              cmp dl, 1
+              je box_down
+              jmp game_loop
+
+move_left:    call clear_player
+              shl word [player_col], 1
+              call set_player
+              call collision
+              cmp cl, 1
+              je move_right
+              cmp dl, 1
+              je box_left
+              jmp game_loop
+
+move_right:   call clear_player
+              shr word [player_col], 1
+              call set_player
+              call collision
+              cmp cl, 1
+              je move_left
+              cmp dl, 1
+              je box_right
+              jmp game_loop
+
+box_up:       call clear_box
+              xor byte [box + bx], cl
+              sub bx, 2
+              ;pusha
+              and cl, byte [box + bx]
+              cmp cl, 0
+              jne move_down
+              ;popa
+              ;and cl, byte [map + bx]
+              ;cmp cl, 0
+              ;jne move_down
+
+              call clear_box
+              sub bx, 2
+              or byte [box + bx], cl
+              jmp game_loop
+
+box_down:
+              ; 28 byte (x4 160)
+              call clear_box
+              xor byte [box + bx], cl
+              add bx, 2
+              ;pusha
+              and cl, byte [box + bx]
+              cmp cl, 0
+              jne move_up
+              ;popa
+              ;and cl, byte [map + bx]
+              ;cmp cl, 0
+              ;jne move_up
+
+              call clear_box
+              add bx, 2
+              or byte [box + bx], cl
+              jmp game_loop
+
+
+
+box_right:    call clear_box
+              xor byte [box + bx], cl
+              shr cx, 1
+              ;pusha
+              and cl, byte [box + bx]
+              cmp cl, 0
+              jne move_left
+              ;popa
+              
+              
+              ;and cl, byte [map + bx]
+              ;cmp cl, 0
+              ;jne move_left
+
+
+              call clear_box
+              shr cx, 1
+              or byte [box + bx], cl
+              jmp game_loop
+
+box_left:     call clear_box
+              xor byte [box + bx], cl
+              shl cx, 1
+              ;pusha
+              and cl, byte [box + bx]
+              cmp cl, 0
+              jne move_right
+              
+              call clear_box
+              shl cx, 1
+              or byte [box + bx], cl
+              jmp game_loop
+
+collision:    mov cx, word [player_col]
+              and ch, byte [map + bx]
+              cmp ch, 0
+              jne hit_wall
+              mov cl, 0
+              mov cx, word [player_col]
+              and ch, byte [box + bx]
+              cmp ch, 0
+              jne hit_box
+              mov dl, 0
+              ret
+hit_wall:     mov cl, 1
+              ret
+hit_box:      mov dl, 1
+              ret
+
+clear_player: mov bl, byte [player_row]
+              and word [box + bx], 0x00ff
+              ret
+
+set_player:   mov bl, byte [player_row]
+              mov cx, word [player_col]
+              or word [box + bx], cx
+              ret
+
+clear_box:    call set_player
+              shr cx, 8
+              xor byte [box + bx], cl
+              ret
+
+clear_screen: mov ax, 0x0000
+              stosw
+              cmp di, 0xb48
+              je done_clear
+              jmp clear_screen
+done_clear:   ret
+
+print_board:
+write_row:    add di, 0x90
+              lodsw
+              cmp al, 0xee
+              je end_write
+              mov bl, 0x80
+              mov cl, 0
+write_col:    push ax
+              mov dl, bl
+              and dl, al
+              cmp dl, 0
+              jne print_wall
+              mov dl, bl
+              and dl, ah
+              cmp dl, 0
+              jne print_dest
+              add di, 2
+              jmp write_next
+print_wall:   cmp bh, 1
+              je print_box
+              mov ax, 0xe1b2
+              stosw
+              jmp write_next
+print_box:    ;cmp word [es:di], 0x0c09
+              ;je highlight
+              mov ax, 0x06fe
+              stosw
+              jmp write_next
+highlight:    ;mov ax, 0x02fe
+              ;stosw
+              ;jmp write_next
+print_dest:   cmp bh, 1
+              je print_player
+              mov ax, 0x0c09
+              stosw
+              jmp write_next
+print_player: mov ax, 0x0e01
+              stosw
+              jmp write_next
+write_next:   pop ax
+              shr bl, 1
+              inc cl
+              cmp cl, 8
+              je write_row
+              jmp write_col
+end_write:    ret
+
+
+player_row db 0x04
+player_col dw 0x2000
+map dw 0x003e, 0x00e2, 0x4082, 0x04e2, 0x40b2, 0x08a3, 0x1281, 0x0881, 0x00ff, 0x00ee
+box dw 0x0000, 0x0000, 0x2010, 0x0008, 0x0008, 0x0000, 0x005c, 0x0000, 0x0000, 0x00ee,
+times 510 - ($ - $$) db 0           ; fill trailing zeros to get exactly 512 bytes long binary file
+dw 0xaa55
+
+times (2880 * 512) - ($-$$) db 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/sokoban/sokoban.asm
+++ src/sokoban/sokoban.asm
@@ -1,255 +1,116 @@
-;==========================
-;          PROG_1
-;==========================
-
-[bits 16]                           ; tell NASM to assemble 16-bit code
+[bits 16]
 [org 0x7c00]
 
-              ; video memory
-              mov bx, 0xb800
-              mov es, bx
+game_loop:              mov di, 0x0000                ; point DI to top left on screen
+                        call clear_screen             ; clear video memory
+                        mov di, 0x4b8                 ; print DI one row below board
+                        mov si, player                ; point SI to player array
+                        mov word [low_byte], 0x0004   ; set low byte to box destination graphics index
+                        mov word [high_byte], 0x0006  ; set high byte to player grphics index
+                        call print_map                ; print box destination tiles and player
+                        mov di, 0x4b8                 ; print DI one row below board
+                        mov si, map                   ; point SI to map array
+                        mov word [low_byte], 0x0000   ; set low byte to box graphics index
+                        mov word [high_byte], 0x0002  ; set high byte to wall graphics index
+                        call print_map                ; print boxex and walls
+                        mov ah, 0x00                  ; BIOS code to get a keystroke
+                        int 0x16                      ; wait for keystroke from a user
+                        ;cmp ah, 0x48                 ;
+                        ;je move_up                   ;
+                        ;cmp ah, 0x50                 ;
+                        ;je move_down                 ;
+                        ;cmp ah, 0x4b                 ;
+                        ;je move_left                 ;
+                        ;cmp ah, 0x4d                 ;
+                        ;je move_right                ;
+                        jmp game_loop                 ;
+;=========================================================================================================
+;                                               PRINT MAP
+;=========================================================================================================
 
-              
-game_loop:    mov di, 0x0000
-              call clear_screen
+print_map:              add di, 0x90                  ; point DI to next row
+                        lodsw                         ; load next row to AX (AH=box, AL=wall)
+                        cmp al, 0xee                  ; are there no more rows left?
+                        je done_print                 ; if so printing is done
+                        mov dh, 0x80                  ; init bit to detect map elements (10000000b)
+                        mov cl, 0                     ; reset column counter
+print_tile:             push ax                       ; preserve current row of map/player                        
+                        mov bx, word [high_byte]      ; init high byte graphics
+                        mov dl, dh                    ; copy box/wall elements detection bit
+                        and dl, al                    ; detect wall/player on the map
+                        cmp dl, 0                     ; found a one?
+                        jne draw_tile                 ; if so then print it
+                        mov bx, word [low_byte]       ; draw graphics for low byte next
+                        mov dl, dh                    ; copy destination/player elements detection bit
+                        and dl, ah                    ; detect box/destination on the map
+                        cmp dl, 0                     ; found one?
+                        jne draw_tile                 ; if so then print it
+                        mov bx, word [high_byte]      ; draw graphics for high byte next
+                        add di, 2                     ; print nothing but skip empty tile
+next_tile:              pop ax                        ; restore current row map/player
+                        shr dh, 1                     ; shift detection bit to the next tile
+                        inc cl                        ; increment tile counter
+                        cmp cl, 8                     ; if no more tiles left in the row
+                        je print_map                  ; go to next row
+                        jmp print_tile                ; otherwise print next tile in the row
+done_print:             ret                           ; return from procedure
+draw_tile:              cmp word [es:di], 0x0c09      ; box occupies it's destination tile
+                        je highlight_box              ; if so then highlight it
+                        mov ax, word [graphics + bx]  ; pick up tile graphics
+                        stosw                         ; draw tile
+                        jmp next_tile                 ; continue print routine
+highlight_box:          mov ax, 0x02fe                ; pick up highlight graphics
+                        stosw                         ; highlight box
+                        jmp next_tile                 ; continue print routine
+;=========================================================================================================
+;                                              CLEAR SCREEN
+;=========================================================================================================
+clear_screen:           mov bx, 0xb800                ; point BX to video memory
+                        mov es, bx                    ; point ES to video memory
+clear_next_byte:        mov ax, 0x0000                ; zero word to clear chars and attrs        
+                        stosw                         ; erase cell in video memory (clear screen)
+                        cmp di, 0x0fa0                ; visidhe screen has been exhausted?
+                        je done_clear                 ; if so then return
+                        jmp clear_next_byte           ; otherwise clear next byte
+done_clear:             ret                           ; retyrn from procedure
+;=========================================================================================================
+;                                               GAME DATA
+;=========================================================================================================
+graphics:               dw 0x06fe                     ; box tile
+                        dw 0xe1b2                     ; wall tile
+                        dw 0x0c09                     ; box destination tile
+                        dw 0x0e01                     ; player tile
+low_byte:               dw 0x00                       ; low byte graphics index (wall/player)
+high_byte:              dw 0x00                       ; high byte graphics index (box/destination)
+player_col:             db 0x20                       ; current player's X position (00100000b)
+player_row:             db 0x04                       ; current player's Y position (player + 4b)
+map:                    dw 0x003e                     ; boxes: 00000000    walls: 00111110
+                        dw 0x00e2                     ;        00000000           11100010
+                        dw 0x1082                     ;        00010000           10000010
+                        dw 0x08e2                     ;        00001000           11100010
+                        dw 0x08b2                     ;        00001000           10110010
+                        dw 0x00a3                     ;        00000000           10100011
+                        dw 0x5c81                     ;        01011100           10000001
+                        dw 0x0081                     ;        00000000           10000001
+                        dw 0x00ff                     ;        00000000           11111111
+                        dw 0x00ee                     ; end of map array marker
+player:                 dw 0x0000                     ; dest:  00000000   player: 00000000
+                        dw 0x0000                     ;        00000000           00000000
+                        dw 0x4020                     ;        01000000           00100000
+                        dw 0x0400                     ;        00000100           00000000
+                        dw 0x4000                     ;        01000000           00000000
+                        dw 0x0800                     ;        00001000           00000000
+                        dw 0x1200                     ;        00010010           00000000
+                        dw 0x0800                     ;        00001000           00000000
+                        dw 0x0000                     ;        00000000           00000000
+                        dw 0x00ee                     ; end of player array marker
+;=========================================================================================================
+;                                            BYTES PADDING
+;=========================================================================================================
+times 510 - ($-$$)      db 0                          ; boot sector padding
+                        dw 0xaa55                     ; BIOS boot signature
+times 1474560 - ($-$$)  db 0                          ; floppy image padding
 
-              mov bh, 0       ; map flag
-              mov di, 0x4b8   ; print at center 548
-              mov si, map
-              call print_board
-
-              ; print boxes
-              mov bh, 1       ; boxes flag
-              mov di, 0x4b8   ; print at center 548
-              mov si, box
-              call print_board
-              
-              xor bx, bx
-              
-              mov ah, 0x00
-              int 0x16
-              
-              
-              
-              cmp ah, 0x48
-              je move_up
-              
-              cmp ah, 0x50
-              je move_down
-              
-              cmp ah, 0x4b
-              je move_left
-              
-              cmp ah, 0x4d
-              je move_right
-
-                 
-              jmp game_loop
-
-move_up:      call clear_player
-              sub byte [player_row], 0x02
-              call set_player
-              call collision
-              cmp cl, 1
-              je move_down
-              cmp dl, 1
-              je box_up
-              jmp game_loop
-
-move_down:    call clear_player
-              add byte [player_row], 0x02
-              call set_player
-              call collision
-              cmp cl, 1
-              je move_up
-              cmp dl, 1
-              je box_down
-              jmp game_loop
-
-move_left:    call clear_player
-              shl word [player_col], 1
-              call set_player
-              call collision
-              cmp cl, 1
-              je move_right
-              cmp dl, 1
-              je box_left
-              jmp game_loop
-
-move_right:   call clear_player
-              shr word [player_col], 1
-              call set_player
-              call collision
-              cmp cl, 1
-              je move_left
-              cmp dl, 1
-              je box_right
-              jmp game_loop
-
-box_up:       call clear_box
-              xor byte [box + bx], cl
-              sub bx, 2
-              ;pusha
-              and cl, byte [box + bx]
-              cmp cl, 0
-              jne move_down
-              ;popa
-              ;and cl, byte [map + bx]
-              ;cmp cl, 0
-              ;jne move_down
-
-              call clear_box
-              sub bx, 2
-              or byte [box + bx], cl
-              jmp game_loop
-
-box_down:
-              ; 28 byte (x4 160)
-              call clear_box
-              xor byte [box + bx], cl
-              add bx, 2
-              ;pusha
-              and cl, byte [box + bx]
-              cmp cl, 0
-              jne move_up
-              ;popa
-              ;and cl, byte [map + bx]
-              ;cmp cl, 0
-              ;jne move_up
-
-              call clear_box
-              add bx, 2
-              or byte [box + bx], cl
-              jmp game_loop
-
-
-
-box_right:    call clear_box
-              xor byte [box + bx], cl
-              shr cx, 1
-              ;pusha
-              and cl, byte [box + bx]
-              cmp cl, 0
-              jne move_left
-              ;popa
-              
-              
-              ;and cl, byte [map + bx]
-              ;cmp cl, 0
-              ;jne move_left
-
-
-              call clear_box
-              shr cx, 1
-              or byte [box + bx], cl
-              jmp game_loop
-
-box_left:     call clear_box
-              xor byte [box + bx], cl
-              shl cx, 1
-              ;pusha
-              and cl, byte [box + bx]
-              cmp cl, 0
-              jne move_right
-              
-              call clear_box
-              shl cx, 1
-              or byte [box + bx], cl
-              jmp game_loop
-
-collision:    mov cx, word [player_col]
-              and ch, byte [map + bx]
-              cmp ch, 0
-              jne hit_wall
-              mov cl, 0
-              mov cx, word [player_col]
-              and ch, byte [box + bx]
-              cmp ch, 0
-              jne hit_box
-              mov dl, 0
-              ret
-hit_wall:     mov cl, 1
-              ret
-hit_box:      mov dl, 1
-              ret
-
-clear_player: mov bl, byte [player_row]
-              and word [box + bx], 0x00ff
-              ret
-
-set_player:   mov bl, byte [player_row]
-              mov cx, word [player_col]
-              or word [box + bx], cx
-              ret
-
-clear_box:    call set_player
-              shr cx, 8
-              xor byte [box + bx], cl
-              ret
-
-clear_screen: mov ax, 0x0000
-              stosw
-              cmp di, 0xb48
-              je done_clear
-              jmp clear_screen
-done_clear:   ret
-
-print_board:
-write_row:    add di, 0x90
-              lodsw
-              cmp al, 0xee
-              je end_write
-              mov bl, 0x80
-              mov cl, 0
-write_col:    push ax
-              mov dl, bl
-              and dl, al
-              cmp dl, 0
-              jne print_wall
-              mov dl, bl
-              and dl, ah
-              cmp dl, 0
-              jne print_dest
-              add di, 2
-              jmp write_next
-print_wall:   cmp bh, 1
-              je print_box
-              mov ax, 0xe1b2
-              stosw
-              jmp write_next
-print_box:    ;cmp word [es:di], 0x0c09
-              ;je highlight
-              mov ax, 0x06fe
-              stosw
-              jmp write_next
-highlight:    ;mov ax, 0x02fe
-              ;stosw
-              ;jmp write_next
-print_dest:   cmp bh, 1
-              je print_player
-              mov ax, 0x0c09
-              stosw
-              jmp write_next
-print_player: mov ax, 0x0e01
-              stosw
-              jmp write_next
-write_next:   pop ax
-              shr bl, 1
-              inc cl
-              cmp cl, 8
-              je write_row
-              jmp write_col
-end_write:    ret
-
-
-player_row db 0x04
-player_col dw 0x2000
-map dw 0x003e, 0x00e2, 0x4082, 0x04e2, 0x40b2, 0x08a3, 0x1281, 0x0881, 0x00ff, 0x00ee
-box dw 0x0000, 0x0000, 0x2010, 0x0008, 0x0008, 0x0000, 0x005c, 0x0000, 0x0000, 0x00ee,
-times 510 - ($ - $$) db 0           ; fill trailing zeros to get exactly 512 bytes long binary file
-dw 0xaa55
-
-times (2880 * 512) - ($-$$) db 0
 
 
 
